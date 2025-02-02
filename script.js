3709bca8d0b9a1e37255cabeb339427fd20f3369
MeghalTiwari <tiwarimeghal@gmail.com>
2025-02-02 15:05:16 +0000

    Adding captcha validation

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-function beforeSubmit() {
-    let outputdate = document.querySelector(".outputdate");
-    let inputdate = document.querySelector(".inputdate");
-    console.log('Input Date', inputdate.value);
-    let formattedDate = new Date(inputdate.value).toLocaleDateString("Localeen-GB");
-    console.log('formattedDate', formattedDate);
-    outputdate.value = formattedDate;
+let captchachecked = false;
+function beforeSubmit(event) {
+    if (captchachecked) {
+        let outputdate = document.querySelector(".outputdate");
+        let inputdate = document.querySelector(".inputdate");
+        console.log('Input Date', inputdate.value);
+        let formattedDate = new Date(inputdate.value).toLocaleDateString("Localeen-GB");
+        console.log('formattedDate', formattedDate);
+        outputdate.value = formattedDate;
+    }
+    else {
+        alert("Please verify your identity uisng recaptcha");
+        event.preventDefault();
+    }
+
 }
 function timestamp() { var response = document.getElementById("g-recaptcha-response"); if (response == null || response.value.trim() == "") { var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value); elems["ts"] = JSON.stringify(new Date().getTime()); document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } } setInterval(timestamp, 500);
+
+function captchasuccess() {
+    captchachecked = true;
+}
